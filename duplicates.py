20315a9c3bf701689630baee6cad6239ad9390e6
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-24 21:38:45 +0300

    add checking catalog or not

--- duplicates.py
+++ duplicates.py
@@ -15,22 +15,25 @@ def find_all_filenames(dirname):
 
 
 def get_similar_filenames(all_filenames):
-    similar_filenames = {}
-    for keys, values in all_filenames.items():
-        if len(values) > 1:
-            similar_filenames.update({keys[0]: values})
-    return json.dumps(similar_filenames, indent=2, ensure_ascii=False)
+    duplicate_filenames = {}
+    for file_info, path in all_filenames.items():
+        if len(path) > 1:
+            duplicate_filenames.update({file_info[0]: path})
+    return json.dumps(duplicate_filenames, indent=2, ensure_ascii=False)
 
 
-def print_similar_files(kind_of_files, similar_filenames):
-    print(kind_of_files, similar_filenames)
+def print_similar_files(kind_of_files, duplicate_filenames):
+    print(kind_of_files, duplicate_filenames)
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         dirname = sys.argv[1]
     else:
-        exit("Не выбран файл")
+        exit("Не выбран каталог")
+    if os.path.isfile(dirname):
+        exit("Ошибка! Передан файл, а не каталог")
     all_filenames = find_all_filenames(dirname)
-    similar_filenames = get_similar_filenames(all_filenames)
-    print_similar_files("Найденные дубликаты:", similar_filenames)
+    duplicate_filenames = get_similar_filenames(all_filenames)
+    print_similar_files("Найденные дубликаты:", duplicate_filenames)
+
