b53d51bab55590906d516e93471516c30f2999f0
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-24 17:47:56 +0300

    fix problems with input directory name

--- duplicates.py
+++ duplicates.py
@@ -1,54 +1,36 @@
 import os
-from collections import Counter
+from collections import Counter, defaultdict
+import json
+import sys
 
 
-def get_input():
-    dirname = input("Введите путь\n")
-    return dirname
+def find_all_filenames(dirname):
+    all_filenames_dict = defaultdict(list)
+    for directory, dirs, filenames in os.walk(dirname):
+        for filename in filenames:
+            path = os.path.join(directory, filename)
+            size_of_file = os.path.getsize(path)
+            all_filenames_dict[(filename, size_of_file)].append(path)
+    return all_filenames_dict
 
 
-def get_similar_dirs(dirname):
-    similar_dirs_buffer = []
-    all_files = os.walk(dirname)
-    for dir_items in list(zip(*all_files)):
-        pass
-    for every_item in dir_items:
-        for items in every_item:
-            similar_dirs_buffer.append(items)
-    similar_dirs = Counter(similar_dirs_buffer)
-    return similar_dirs.most_common()
+def get_similar_filenames(all_filenames):
+    similar_filenames = {}
+    for keys, values in all_filenames.items():
+        if len(values) > 1:
+            similar_filenames.update({keys[0]: values})
+    return json.dumps(similar_filenames, indent=2, ensure_ascii=False)
 
 
-def find_all_files(dirname):
-    all_files_buffer = []
-    for directory, dirs, files in os.walk(dirname):
-        for file in files:
-            path = os.path.join(directory, file)
-            all_files_buffer.append(path)
-    return all_files_buffer
-
-
-def print_similar_files(files, all_files):
-    link_buffer = []
-    size_buffer = []
-    for link_item in files:
-        for dir_item in all_files:
-            if link_item[0] in dir_item:
-                size_of_file = os.path.getsize(dir_item)
-                link_buffer.append(dir_item)
-                size_buffer.append(size_of_file)
-        if link_item[1] == 1:
-            break
-    for buffer_index, link in enumerate(link_buffer[:-2]):
-        print(
-            link,
-            "Размер файла в байтах: {}"
-            .format(size_buffer[buffer_index])
-        )
+def print_similar_files(kind_of_files, similar_filenames):
+    print(kind_of_files, similar_filenames)
 
 
 if __name__ == "__main__":
-    dirname = get_input()
-    all_files = find_all_files(dirname)
-    similar_files = get_similar_dirs(dirname)
-    print_similar_files(similar_files, all_files)
+    if len(sys.argv) > 1:
+        dirname = sys.argv[1]
+    else:
+        exit("Не выбран файл")
+    all_filenames = find_all_filenames(dirname)
+    similar_filenames = get_similar_filenames(all_filenames)
+    print_similar_files("Найденные дубликаты:", similar_filenames)
